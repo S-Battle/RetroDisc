1df22132dd41ba7c1bd0efb020603cb3eb57ef26
S-Battle <sethbattle@gmail.com>
2025-06-05 18:26:33 -0700

    another push

---
+++ frontend/retro_disc/src/pages/AdminPage.jsx
@@ -0,0 +1,100 @@
+import React, {useState, useEffect} from  "react";
+
+
+
+
+const AdminPage = ({urlFix}) => {
+    const [artistInput, setArtistInput] = useState("");
+    const [albumInput, setAlbumInput] = useState("");
+    const [priceInput, setPriceInput] = useState("");
+    const [genreInput, setGenreInput] = useState("");
+    const [yearInput, setYearInput] = useState("");
+
+    const allAlbumsView = ()=>{
+
+        return <>
+        <div style={{width:'60vw', border:"2px solid black", height: "60vh", overflow:"auto"}}></div>        
+        </>
+    } 
+    const addAlbumFunction = async ()=>{
+
+        const response = await fetch(`${urlFix}/api/add/album`, {
+            method: "POST",
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify({
+                artistInput: artistInput,
+                albumInput: albumInput,
+                priceInput: priceInput,
+                genreInput: genreInput,
+                yearInput: yearInput
+            })
+        })
+        const data = await response.json();
+    }
+    
+    
+
+
+    const addedAlbumView = ()=>{
+
+        return<> 
+            <div style={{display:"flex", }}></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <label htmlFor="">ARTIST <input type="text"  value={artistInput}  onChange={(e)=>{                
+                setArtistInput(()=>{
+                    return e.target.value;
+                })
+            }}     /></label>
+            <label htmlFor="">ALBUM <input type="text" value={albumInput} onChange={(e)=>{                  
+                setAlbumInput(()=>{
+                    return e.target.value
+                })               
+            }} /></label>
+            <label htmlFor="">PRICE <input type="number" value={priceInput} onChange={(e)=>{               
+                setPriceInput(()=>{
+                    return e.target.valueAsNumber;
+                })
+            }}  /></label>
+            <label htmlFor="">GENRE <input type="text" value={genreInput}  onChange={(e)=>{                
+                setGenreInput(()=>{
+                    return e.target.value;
+                })
+            }} /></label>
+            <label htmlFor="">YEAR <input type="number" value={yearInput}   onChange={(e)=>{                
+                setYearInput(()=>{
+                    return e.target.valueAsNumber;
+                })
+            }} /></label>
+        </>
+
+    }
+
+
+          return(
+                             <>
+                             <div style={{display:"flex", flexDirection:"column", justifyContent:"center",  alignItems:'center'}}>
+                             <h1>ADMIN PAGE</h1>
+
+                             <button onClick={()=>{
+                                addAlbumFunction();
+                             }}>ADD ALBUM</button>
+                             <button onClick={()=>{
+                                viewAllAlbums();
+                             }}>GET ALBUMS</button>
+                             {addedAlbumView()}
+                             {allAlbumsView()}
+                             
+
+                            </div>
+
+                             </>
+          );
+
+}
+
+
+
+export default AdminPage;
